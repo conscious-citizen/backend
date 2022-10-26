4f734336b061ae3bbec242bac4ccab55e98955cc
EVM <suhov.toxa@gmail.com>
2022-10-26 10:01:45 +0400

    Tblue-7.6-authorizationApi:JWTtoken

--- src/main/java/ru/ssau/citizen/service/ActorService.java
+++ src/main/java/ru/ssau/citizen/service/ActorService.java
@@ -1,8 +1,13 @@
 package ru.ssau.citizen.service;
 
+import ru.ssau.citizen.dto.JwtResponseDto;
+import ru.ssau.citizen.dto.LoginDto;
+import ru.ssau.citizen.dto.MessageResponse;
+import ru.ssau.citizen.dto.RegistrationDto;
 import ru.ssau.citizen.entities.Actor;
 
 public interface ActorService {
-    void save(Actor actor);
+    JwtResponseDto login(LoginDto loginDto);
+    MessageResponse registration(RegistrationDto registrationDto);
     Actor findActorByLogin(String login);
 }
