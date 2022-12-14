f88b6c96cdf6a13b9310f220bb3301f4f0fe5113
EVM <suhov.toxa@gmail.com>
2022-12-15 02:46:22 +0400

    CreateDraft:CreateDraftFix Event photo to base

--- src/main/java/ru/ssau/citizen/repository/PhotoRepository.java
+++ src/main/java/ru/ssau/citizen/repository/PhotoRepository.java
@@ -1,2 +1,8 @@
-package ru.ssau.citizen.repository;public interface PhotoReposytory {
+package ru.ssau.citizen.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import ru.ssau.citizen.entities.Photo;
+@Repository
+public interface PhotoRepository extends JpaRepository<Photo, Long> {
 }
